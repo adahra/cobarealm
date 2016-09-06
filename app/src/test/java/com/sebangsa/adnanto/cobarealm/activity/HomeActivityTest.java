262d4eaaa90eeffed28ba17f68af3b9efead1f9f
adahra <adnantoa@gmail.com>
2016-09-06 16:18:27 +0700

    perubahan pada activity

---
+++ app/src/test/java/com/sebangsa/adnanto/cobarealm/activity/HomeActivityTest.java
@@ -0,0 +1,19 @@
+package com.sebangsa.adnanto.cobarealm.activity;
+
+import org.junit.Test;
+
+/**
+ * Created by adnanto on 9/6/16.
+ */
+public class HomeActivityTest {
+
+    @Test
+    public void testOnCreate() throws Exception {
+
+    }
+
+    @Test
+    public void testSetRecyclerView() throws Exception {
+
+    }
+}
