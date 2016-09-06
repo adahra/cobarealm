5e1287c29ff7576e0a6120143b6b8956621c000f
adahra <adnantoa@gmail.com>
2016-09-06 16:21:04 +0700

    perubahan pada tampilan home

--- app/src/main/java/com/sebangsa/adnanto/cobarealm/activity/HomeActivity.java
+++ app/src/main/java/com/sebangsa/adnanto/cobarealm/activity/HomeActivity.java
@@ -8,8 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import com.facebook.stetho.Stetho;
 import com.sebangsa.adnanto.cobarealm.R;
 import com.sebangsa.adnanto.cobarealm.adapter.AdapterArticle;
 import com.sebangsa.adnanto.cobarealm.base.RealmHelper;
@@ -20,33 +22,30 @@ import java.util.ArrayList;
 /**
  * Created by adnanto on 9/6/16.
  */
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = HomeActivity.class.getSimpleName();
 
     private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
     private RealmHelper realmHelper;
     private ArrayList<ArticleModel> articleModelArrayList;
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        Log.d(TAG, "onCreate() started");
         setContentView(R.layout.base_app_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Stetho.initializeWithDefaults(HomeActivity.this);
 
         articleModelArrayList = new ArrayList<>();
         realmHelper = new RealmHelper(this);
         recyclerView = (RecyclerView) findViewById(R.id.rv_article);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), TambahActivity.class));
-                finish();
-            }
-        });
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(this);
 
         setRecyclerView();
     }
@@ -60,17 +59,23 @@ public class HomeActivity extends AppCompatActivity {
 
         AdapterArticle adapterArticle = new AdapterArticle(articleModelArrayList,
                 new AdapterArticle.OnItemClickListener() {
-                   @Override
+                    @Override
                     public void onClick(ArticleModel articleModel) {
-                       Intent intent = new Intent(getApplicationContext(), EditActivity.class);
-                       intent.putExtra("id", articleModel.getDescription());
-                       intent.putExtra("title", articleModel.getTitle());
-                       intent.putExtra("description", articleModel.getDescription());
-                       startActivity(intent);
-                       finish();
-                     }
+                        Intent intent = new Intent(HomeActivity.this, EditActivity.class);
+                        intent.putExtra("id", articleModel.getDescription());
+                        intent.putExtra("title", articleModel.getTitle());
+                        intent.putExtra("description", articleModel.getDescription());
+                        startActivity(intent);
+                        finish();
+                    }
                 });
 
         recyclerView.setAdapter(adapterArticle);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(HomeActivity.this, TambahActivity.class));
+        finish();
     }
 }
